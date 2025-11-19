d420996ef204b070aa3fc4ba73dd47bfb9419ab9
hasbi-alaziz <94413730+hasbi-alajiz@users.noreply.github.com>
2025-11-19 13:26:04 +0700

    test pass (kinda?)

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+  if (times < 0) return 'ERROR';
+  let string = '';
+  for(let i = 0; i < times; i++); {
+    string += word;
+  } 
+  return repeatString;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+repeatString('hey', 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
